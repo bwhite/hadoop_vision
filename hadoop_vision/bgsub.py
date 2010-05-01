882602aa0419f0ae73dc72ac82d83e81319591ac
brandyn <brandyn@brandynd.cs.umd.edu>
2010-04-30 21:21:15 -0400

    Works

--- hadoop_vision/bgsub.py
+++ hadoop_vision/bgsub.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-import os
-import cPickle as pickle
+import base64
 
 import numpy as np
+import StringIO
+import Image
 
 import hadoopy
 
@@ -11,31 +12,41 @@ class Mapper(object):
 
     @staticmethod
     def _compute_blockid(image_id):
-        images_in_block = 300
-        returun image_id / images_in_block
+        images_in_block = 500
+        return str(int(image_id) / images_in_block)
         
-    def map(self, image_id, image):
+    def map(self, video_id, value):
+        image_id, image = value
         block_id = self._compute_blockid(image_id)
-        yield (block_id, 1), (image_id, image)
-        yield (block_id, 2), (image_id, image)
+        video_image_id = '-'.join((video_id, str(image_id)))
+        video_block_flag_id = '%s-%s\t' %(video_id, block_id)
+        yield video_block_flag_id + '1', (video_image_id, image)
+        yield video_block_flag_id + '2', (video_image_id, image)
+
+def reducer(key, values):
+    for value in values:
+        yield key, value
 
 class Reducer(object):
 
     def reduce(self, key, values):
-        block_id, flag = key
-        if flag == 1:
-            self._handle_flag1(values)
-        else:
-            self._handle_flag2(values)
+        return self._handle_flag1(values) if key[-1] == '1' else self._handle_flag2(values)
+
+    @staticmethod
+    def _load_image(image):
+        image = Image.open(StringIO.StringIO(image)).convert('L').tostring()
+        image = np.fromstring(image, dtype=np.uint8)
+        return np.array(image, np.float64)
 
     def _handle_flag1(self, values):
+        c, s, ss = None, None, None
         for image_id, image in values:
-            image = np.fromstring(image, dtype=np.float32)
+            image = self._load_image(image)
             try:
                 c += 1
                 s += image
                 ss += image * image
-            except NameError:
+            except TypeError:
                 c = 1
                 s = image
                 ss = image * image
@@ -44,12 +55,12 @@ class Reducer(object):
 
     def _handle_flag2(self, values):
         for image_id, image in values:
-            image = np.fromstring(image, dtype=np.float32)
+            image = self._load_image(image)
             diff = image - self.m
-            b = diff * diff < 6.25 * self.v
+            b = (diff * diff > 6.25 * self.v) * np.uint8(255)
             yield image_id, b.tostring()
 
 
 if __name__ == "__main__":
-    if hadoopy.run(Mapper, reducer):
+    if hadoopy.run(Mapper, Reducer):
         hadoopy.print_doc_quit(__doc__)
---
+++ hadoop_vision/runbg.py
@@ -0,0 +1,4 @@
+import hadoopy
+hadoopy.freeze(script_path='bgsub.py',
+               remove_dir=True)
+hadoopy.run_hadoop('/tmp/bwhite/input/pets2006.video_frame_data.tb', '/tmp/bwhite/output/pets2006.video_frame_data.b0', 'bgsub.py', partitioner='org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner', jobconfs=['mapred.text.key.partitioner.options=-k1,1', 'mapred.reduce.tasks=50'], frozen_path='frozen', compress_output=True) 
