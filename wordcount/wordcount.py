4f1f16f2886d5ebf59224f34b22a397efb6ee694
Brandyn <bwhite@cs.umd.edu>
2010-06-02 15:41:07 -0400

    Changed wordcount variable names to match those in the paper

--- wordcount/wordcount.py
+++ wordcount/wordcount.py
@@ -23,40 +23,42 @@ __licence__ = 'GPL V3'
 import hadoopy
 
 
-def mapper(key, value):
-    """Take in a byte offset and a document, emit word counts.
+class Mapper(object):
+    def map(self, a, d):
+        """Take in a byte offset and a document, emit word counts.
 
-    Args:
-        key: byte offset
-        value: document as a string
+        Args:
+            a: byte offset (unused)
+            d: document as a string of terms delimited by whitespace
 
-    Yields:
-        A tuple in the form of (key, value)
-        key: word (string)
-        value: count (int)
-    """
-    for word in value.split():
-        yield word, 1
+        Yields:
+            A tuple in the form of (key, value)
+            key: term (string)
+            value: partial count (int)
+        """
+        for t in d.split():
+            yield t, 1
 
 
-def reducer(key, values):
-    """Take in an iterator of counts for a word, sum them, and return the sum.
+class Reducer(object):
+    def reduce(self, t, counts):
+        """Take in an iterator of counts for a word, sum them, and return sum.
 
-    Args:
-        key: word (string)
-        values: counts (int)
+        Args:
+            t: word (string)
+            counts: counts (int)
 
-    Yields:
-        A tuple in the form of (key, value)
-        key: word (string)
-        value: count (int)
-    """
-    accum = 0
-    for count in values:
-        accum += int(count)
-    yield key, accum
+        Yields:
+            A tuple in the form of (key, value)
+            key: term (string)
+            value: count (int)
+        """
+        _sum = 0
+        for c in counts:
+            _sum += c
+        yield t, _sum
 
 
 if __name__ == "__main__":
-    if hadoopy.run(mapper, reducer):
+    if hadoopy.run(Mapper, Reducer):
         hadoopy.print_doc_quit(__doc__)
