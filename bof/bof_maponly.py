e0c28202757fd483eb20d6914061936ca0b37e5e
Brandyn <bwhite@cs.umd.edu>
2010-06-04 18:19:53 -0400

    BoF looks good and passes tests

---
+++ bof/bof_maponly.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# (C) Copyright 2010 Brandyn A. White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Hadoopy Bag-of-Features (BoF)
+"""
+
+__author__ = 'Brandyn A. White <bwhite@cs.umd.edu>'
+__license__ = 'GPL V3'
+
+import pickle
+
+import hadoopy
+import numpy as np
+
+
+class Mapper(object):
+    def _load_clusters(self):
+        with open('clusters.pkl') as fp:
+            return pickle.load(fp)
+
+    def _nearest_cluster_id(self, clusters, point):
+        """Find L2 squared nearest neighbor
+
+        Args:
+            clusters: A numpy array of shape (M, N). (N=Dims, M=NumClusters)
+            point: A numpy array of shape (N,) or (1, N). (N=Dims)
+        Returns:
+            An int representing the nearest neighbor index into clusters.
+        """
+        dist = point - clusters
+        dist = np.sum(dist * dist, 1)
+        return int(np.argmin(dist))
+
+    def _update_histogram(self, clusterid, histogram):
+        try:
+            histogram[clusterid] += 1
+        except KeyError:
+            histogram[clusterid] = 1
+
+    def configure(self):
+        self.clusters = self._load_clusters()
+
+    def map(self, imageid, features):
+        """
+
+        Args:
+            imageid: An ID that is directly passed to the output
+            features: As a list of numpy arrays
+
+        Yields:
+            A tuple in the form of (key, value)
+            key: imageid
+            value: histogram as a dict of (dim, val) (int, int)
+        """
+        histogram = {}
+        for feature in features:
+            clusterid = self._nearest_cluster_id(self.clusters, feature)
+            self._update_histogram(clusterid, histogram)
+        yield imageid, histogram
+
+if __name__ == "__main__":
+    if hadoopy.run(Mapper):
+        hadoopy.print_doc_quit(__doc__)
---
+++ bof/bof_test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# (C) Copyright 2010 Brandyn A. White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test BoF
+"""
+
+__author__ = 'Brandyn A. White <bwhite@cs.umd.edu>'
+__license__ = 'GPL V3'
+
+import unittest
+import hadoopy
+import os
+import pickle
+import numpy as np
+
+from bof import Mapper, Reducer
+
+
+class Test(hadoopy.Test):
+    def __init__(self, *args, **kw):
+        super(Test, self).__init__(*args, **kw)
+
+    def test_map(self):
+        clusters = [np.array([4., 4.]),
+                    np.array([6., 6.])]
+        with open('clusters.pkl', 'w') as fp:
+            pickle.dump(clusters, fp, 2)
+        test_in = []
+        test_in.append((0, [np.array([0., 0.]),
+                            np.array([1., 1.]),
+                            np.array([2., 2.]),
+                            np.array([2., 3.]),
+                            np.array([10., 10.])]))
+        test_in.append((1, [np.array([10., 10.]),
+                            np.array([11., 11.]),
+                            np.array([12., 12.]),
+                            np.array([12., 13.]),
+                            np.array([0., 1.])]))
+        test_in.append((2, [np.array([10., 10.]),
+                            np.array([11., 11.]),
+                            np.array([12., 12.]),
+                            np.array([12., 13.])]))
+        new_in = []
+        for i, j in test_in:
+            for k in j:
+                new_in.append((i, k))
+        test_in = new_in
+        test_out = []
+        test_out.append((0, {0: 4, 1: 1}))
+        test_out.append((1, {0: 1, 1: 4}))
+        test_out.append((2, {1: 4}))
+        kv = self.shuffle_kv(self.call_map(Mapper, test_in))
+        self.assertEqual(self.call_reduce(Reducer, kv), test_out)
+
+if __name__ == '__main__':
+    unittest.main()
